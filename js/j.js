8274d61cbf4e20680d71fa64d842109f9df1f569
Zoulou India <zixzizina@gmail.com>
2025-09-06 12:05:57 +0300

    premier commit avec index.html

---
+++ js/j.js
@@ -0,0 +1,21 @@
+/*const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+	slides[currentSlide].classList.remove('visible');
+	currentSlide = index;
+	slides[currentSlide].classList.add('visible');
+}
+
+function nextSlide() {
+	const nextIndex = (currentSlide + 1) % slides.length;
+	showSlide(nextIndex);
+}
+
+function prevSlide() {
+	const prevIndex = (currentSlide - 1 + slides.length) % slides.length;
+	showSlide(prevIndex);
+}
+
+slides[currentSlide].classList.add('visible');
+*/
